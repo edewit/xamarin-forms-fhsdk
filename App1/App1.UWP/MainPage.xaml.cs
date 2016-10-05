bf75e8bf1b02969f96b6a30c83c49e706b4a0420
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-10-05 14:21:22 +0200

    Add project files.

---
+++ App1/App1.UWP/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml.Navigation;
+using FHSDKPortable;
+
+namespace App1.UWP
+{
+    public sealed partial class MainPage
+    {
+        private readonly App1.App _app;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _app = new App1.App();
+            LoadApplication(_app);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await FHClient.Init();
+            _app.ProjectReady();
+        }
+    }
+}
